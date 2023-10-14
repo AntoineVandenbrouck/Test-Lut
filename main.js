52e9bc7ce06528349724104943db1f9261bffc1e
AntoineVandenbrouck <129107043+AntoineVandenbrouck@users.noreply.github.com>
2023-10-14 10:07:39 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,27 +1,45 @@
 const { app, BrowserWindow } = require('electron');
 
+let mainWindow;
+
 function createWindow() {
-  const win = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: true
     }
   });
 
-  win.loadFile('index.html');
+  mainWindow.loadFile('index.html');
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
 }
 
-app.whenReady().then(createWindow);
+app.on('ready', createWindow);
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
 });
 
-app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
+app.on('activate', function () {
+  if (mainWindow === null) createWindow();
 });
+
+const { ipcMain, dialog } = require('electron');
+
+ipcMain.on('open-directory-dialog', (event) => {
+    dialog.showOpenDialog(mainWindow, {
+        properties: ['openDirectory']
+    }).then(result => {
+        if (!result.canceled) {
+            event.sender.send('selected-directory', result.filePaths);
+        }
+    }).catch(err => {
+        console.log(err);
+    });
+});
+
